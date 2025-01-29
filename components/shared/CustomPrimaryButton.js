4a31e24ec3e7b3aa5272e03ae44d18bb3c5379bc
Luciano Palacio <luciano.palacio.dev@gmail.com>
2025-01-28 23:38:07 -0300

    fix: fix problem with the sintax on the buttons

--- components/shared/CustomPrimaryButton.js
+++ components/shared/CustomPrimaryButton.js
@@ -3,7 +3,7 @@
 export default function CustomPrimaryButton({text, handleClick}) {
   return (
     <button
-      handleClick={handleClick}
+      onClick={handleClick}
       type="button"
       className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
     >
