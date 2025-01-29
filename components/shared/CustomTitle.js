39480384d0263e60d3e7a3093d964004bb892dd6
lucianopalacio@toptive.co <luciano.palacio.dev@gmail.com>
2025-01-29 18:33:46 -0300

    styles: center text of custom title component

--- components/shared/CustomTitle.js
+++ components/shared/CustomTitle.js
@@ -1,6 +1,6 @@
 export default function CustomTitle({ text }) {
   return (
-    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-center">
       {text}
     </h1>
   );
