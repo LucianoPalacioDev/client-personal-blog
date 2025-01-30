b756fd90c1ec488284cf73d23e06d93d6363ca69
lucianopalacio@toptive.co <luciano.palacio.dev@gmail.com>
2025-01-29 23:17:31 -0300

    refactor: just format file

--- components/profile/menus/PostOptionsMenu.js
+++ components/profile/menus/PostOptionsMenu.js
@@ -13,13 +13,19 @@ export default function PostOptionsMenu({
     >
       <ul className="">
         <li>
-          <button className="w-full flex items-center space-x-3 p-4 hover:bg-gray-100" onClick={handleEditPost}>
+          <button
+            className="w-full flex items-center space-x-3 p-4 hover:bg-gray-100"
+            onClick={handleEditPost}
+          >
             <EditIcon />
             <span>Edit</span>
           </button>
         </li>
         <li>
-          <button className="w-full flex items-center space-x-3 p-4 hover:bg-gray-100" onClick={handleDeletePost}>
+          <button
+            className="w-full flex items-center space-x-3 p-4 hover:bg-gray-100"
+            onClick={handleDeletePost}
+          >
             <DeleteIcon />
             <span>Delete</span>
           </button>
