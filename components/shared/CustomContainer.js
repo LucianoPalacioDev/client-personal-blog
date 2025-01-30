d26f22453679a70c19ab3d67c6a6dfa4356c1859
lucianopalacio@toptive.co <luciano.palacio.dev@gmail.com>
2025-01-30 10:57:06 -0300

    styles: improve the styles of welcome, sign up and login page

--- components/shared/CustomContainer.js
+++ components/shared/CustomContainer.js
@@ -1,7 +1,7 @@
 export default function CustomContainer({ children }) {
   return (
     <div className="w-full sm:w-3/4 md:w-1/2 h-full sm:h-3/4 flex justify-center items-center overflow-y-auto" >
-      <div className="w-full block p-6 bg-white border-none sm:border-solid sm:border sm:border-gray-200 rounded-lg shadow-none sm:shadow-sm ">
+      <div className="w-full h-full block p-6 bg-white border-none sm:border-solid sm:border sm:border-gray-200 rounded-lg shadow-none sm:shadow-sm ">
         {children}
       </div>
     </div>
--- components/sign-up/SignUpView.js
+++ components/sign-up/SignUpView.js
@@ -7,7 +7,7 @@ export default function SignUpView() {
   return (
     <div className="w-full h-full flex flex-1 justify-center items-center ">
       <CustomContainer>
-        <div className="w-full flex flex-col justify-center items-center gap-12">
+        <div className="w-full h-full flex flex-col justify-center items-center gap-12">
           <div className="w-full flex justify-center items-center">
             <CustomTitle text="Sign Up" />
           </div>
